7d55852ee522a7f18fee9fe851ec4c11410052e3
Jorge SB <jorge.trabajo42@gmail.com>
2020-11-16 10:43:20 +0100

    nuevo

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import * as firebase from 'firebase';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'appCompras';
+
+  ngOnInit() {
+    firebase.default.initializeApp({
+      apiKey: "AIzaSyChyxT-FCgA8JPlLHSxYm1S5omrnjKrAlc",
+      authDomain: "crompasapp.firebaseapp.com",
+
+    });
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,62 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProveedoresService } from './servicios/proveedores.service';
+import { ProveedoresComponent } from './proveedores/proveedores/proveedores.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { AddproveeComponent } from './proveedores/addprovee/addprovee.component';
+import { FormsModule } from '@angular/forms';
+import { AddpresComponent } from './presupuestos/addpres/addpres.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { PresupuestosService } from './servicios/presupuestos.service';
+import { PresupuestosComponent } from './presupuestos/presupuestos/presupuestos.component';
+import { EditpresComponent } from './presupuestos/editpres/editpres.component';
+import { RegistroComponent } from './autenticacion/registro/registro.component';
+import { AutenticacionService } from './servicios/autenticacion.service';
+import { InisesComponent } from './autenticacion/inises/inises.component';
+import { GuardService } from './servicios/guard.service';
+import { EditproComponent } from './proveedores/editpro/editpro.component';
+
+const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'proveedores', component: ProveedoresComponent, canActivate:[GuardService]},
+  { path: 'addprovee', component: AddproveeComponent, canActivate:[GuardService]},
+  { path: 'addpres', component: AddpresComponent, canActivate:[GuardService]},
+  { path: 'presupuestos', component: PresupuestosComponent, canActivate:[GuardService]},
+  { path: 'editpres/:id', component: EditpresComponent, canActivate:[GuardService]},
+  { path: 'editpro/:id', component: EditproComponent, canActivate:[GuardService]},
+  { path: 'registro', component: RegistroComponent },
+  { path: 'iniciosesion', component: InisesComponent },
+  { path: '**', component: InicioComponent }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProveedoresComponent,
+    InicioComponent,
+    HeaderComponent,
+    AddproveeComponent,
+    AddpresComponent,
+    PresupuestosComponent,
+    EditpresComponent,
+    RegistroComponent,
+    InisesComponent,
+    EditproComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [ProveedoresService,  PresupuestosService, AutenticacionService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
